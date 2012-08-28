ad9bf73a703314ac9d0172585fe80e808edcc458
Michael Adelson <mike.adelson314@gmail.com>
2012-08-28 19:39:17 -0400

    Call & IsAnonymous helpers

--- LinqToHadoop/LinqToHadoop/Helpers.cs
+++ LinqToHadoop/LinqToHadoop/Helpers.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Reflection;
+using LinqToHadoop.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace LinqToHadoop
 {
@@ -15,12 +17,38 @@ namespace LinqToHadoop
             return methodCallExpression.Method;
         }
 
+        public static MethodCallExpression Call(this MethodInfo @this, Expression instanceOrFirstParameter, params Expression[] parameters)
+        {
+            var parametersToUse = !@this.IsStatic
+                ? parameters
+                : new[] { instanceOrFirstParameter }.Concat(parameters)
+                    .Where(e => e != null);
+            var expression = Expression.Call(
+                instance: !@this.IsStatic ? instanceOrFirstParameter : null,
+                method: @this.IsGenericMethod
+                    ? @this.MakeGenericMethodFromParameters(parametersToUse.Select(e => e.Type).ToList())
+                    : @this,
+                arguments: parametersToUse
+            );
+            return expression;
+        }
+
         public static int GetHashCode<T>(T obj)
         {
             var hash = obj != null
                 ? obj.GetHashCode()
                 : 0;
             return hash;
+        }
+
+        public static bool IsAnonymous(this Type @this)
+        {
+            // HACK: The only way to detect anonymous types right now.
+            var isAnonymous = Attribute.IsDefined(@this, typeof(CompilerGeneratedAttribute), false)
+                && @this.IsGenericType && @this.Name.Contains("AnonymousType")
+                && (@this.Name.StartsWith("<>") || @this.Name.StartsWith("VB$"))
+                && (@this.Attributes & TypeAttributes.NotPublic) == TypeAttributes.NotPublic;
+            return true;
         }
     }
 }
