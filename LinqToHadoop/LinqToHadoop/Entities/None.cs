705ca7479fe9d695410f726b621098abd0c2b226
Michael Adelson <mike.adelson314@gmail.com>
2012-09-02 20:49:22 -0400

    Added a None singleton type which acts like Void

---
+++ LinqToHadoop/LinqToHadoop/Entities/None.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqToHadoop.Entities
+{
+    public class None
+    {
+        public static readonly None Instance = new None();
+
+        private None()
+        {
+        }
+    }
+
+    public static class NoneHelpers
+    {
+        public static KeyValuePair<TKey, None> AsKey<TKey>(this TKey @this)
+        {
+            return @this.WithValue(None.Instance);
+        }
+
+        public static KeyValuePair<None, TValue> AsValue<TValue>(this TValue @this)
+        {
+            return @this.WithKey(None.Instance);
+        }
+    }
+}
